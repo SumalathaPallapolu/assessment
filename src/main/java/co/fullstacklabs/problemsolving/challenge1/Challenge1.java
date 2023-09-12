78a96011a6ae3a37e979c21f6abf81aed0f8c824
Felipe Cardozo <dafelcardozo@gmail.com>
2023-09-12 01:47:26 -0500

    Challenge 1 done.

--- src/main/java/co/fullstacklabs/problemsolving/challenge1/Challenge1.java
+++ src/main/java/co/fullstacklabs/problemsolving/challenge1/Challenge1.java
@@ -1,7 +1,9 @@
 package co.fullstacklabs.problemsolving.challenge1;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Predicate;
 
 
 /**
@@ -11,6 +13,14 @@ import java.util.Map;
  */
 public class Challenge1 {
     public static Map<String, Float> numbersFractionCalculator(Integer[] numbers) {
-        return new HashMap<String, Float>();
+        Map<String, Float> results = new HashMap<String, Float>();
+        results.put("positives", fraction(numbers, n -> n > 0));
+        results.put("negative", fraction(numbers, n -> n < 0));
+        results.put("zeros", fraction(numbers, n -> n == 0));
+        return results;
+    }
+
+    private static float fraction(Integer[] numbers, Predicate<Integer> op) {
+        return ((float) Arrays.stream(numbers).filter(op).count() / (float) numbers.length);
     }
 }
